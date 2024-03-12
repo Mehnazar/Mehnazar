0f7dc26a86c90310168d7623953820793d9fbdeb
Mehnazar <161054226+Mehnazar@users.noreply.github.com>
2024-03-12 07:15:34 +0500

    Add files via upload

---
+++ daynine.js
@@ -0,0 +1,11 @@
+// question 26
+//Alien colors#1: Imagine an alien was just shot down in a game. Assign "green","yellow" or "red" to a variable called alien_color.
+var alien_color = "red";
+if (alien_color == "green") {
+    console.log("you killed an alien, hurrah!");
+}
+// check if killed alien is not green 
+//let alien_color: string = "red";
+else {
+    console.log("wrong shot");
+}
